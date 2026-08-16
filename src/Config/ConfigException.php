46e561aa2f9b8a2667eb86f09f25cec862411fbd
Heitor Althmann <heitoralthmann@gmail.com>
2026-08-15 23:56:32 -0300

    refactor: separate environment configuration loading

--- src/Config/ConfigException.php
+++ src/Config/ConfigException.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace ContaAzulCli\Config;
 
+/** Indicates that required CLI configuration cannot be loaded or resolved. */
 final class ConfigException extends \RuntimeException
 {
 }
